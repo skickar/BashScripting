2acfbc2e71120ec00ac30d9b4482ffdb46129483
Skickar <40251293+skickar@users.noreply.github.com>
2022-01-20 17:25:02 -0700

    generate password list 4 broken brute force lab
    Generates a username and password list for burpsuite "broken brute force protection" lab

--- burplist.sh
+++ burplist.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-## Create the password list by taking burplist.txt and inserting "peter" every other line
+## FIRST: Create a "burplist.txt" file in the same folder as this script & add the passwords from the lab
+## This script will create a "burplistpeter.txt" file with passwords, & a "username.txt" file with usernames
 
+## This line creates the password list by taking burplist.txt and inserting "peter" every other line
 awk ' {print;} NR % 1 == 0 { print "peter"; }'  burplist.txt > burplistpeter.txt
 
-## Create the username list, adding "carlos" and "wiener" 100 times to username.txt
-
+## These lines create the username list by adding "carlos" and "wiener" 100 times to username.txt
 function addName { echo "carlos" >> username.txt; echo "wiener" >> username.txt; }
 for value in {1..100}
 do
